5f8b04a9fab2e2e144a14662aa38eabf143b178d
Dodotree <talkingseed@gmail.com>
2013-10-17 13:22:40 -0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+
+// to test
+// curl -F MAX_FILE_SIZE='10485760' -F userfile=@"/u/test.txt" http://localhost/file.php
+
+$uploaddir = '/var/www/uploads/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "Possible file upload attack!\n";
+}
+
+echo 'Here is some more debugging info:';
+print_r($_FILES);
+
+print "</pre>";
+
+?>
